5f2b85e2687c06fc1c186407bd5c73c4c36bd142
anggarobo <anggaprabes@gmail.com>
2022-06-03 17:36:31 +0700

    variable declaration part 1

--- 01.hello-world/main.go
+++ 01.hello-world/main.go
@@ -3,5 +3,10 @@ package main
 import "fmt"
 
 func main() {
+	var lang1 string = "Go"
+	var lang2 string
+	lang2 = "Javascript"
+
 	fmt.Println("Hello World")
+	fmt.Printf("Hi, My Name is Angga, I'm learning %s and %s programming language!\n", lang1, lang2)
 }
