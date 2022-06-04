2c3357f13020f29303ac2831c7f8e69a2265ee0e
anggarobo <anggaprabes@gmail.com>
2022-06-04 23:55:46 +0700

    variable declaration part 3: multi variable declaration

--- 01.hello-world/main.go
+++ 01.hello-world/main.go
@@ -9,11 +9,18 @@ func main() {
 	lang2 = "Javascript"
 
 	// type interface
-	goFramework := "gin gonic"
+	goFramework := "echo"
 	var jsFramework = "react.js"
+	goFramework = "gin gonic"
+
+	/* multi variable declaration
+	variable declarations can also use type interfaces */
+	var frontend, backend *string
+	frontend, backend = &jsFramework, &goFramework
 
 	fmt.Println("Hello World")
 	fmt.Printf("Hi, My Name is Angga, I'm learning %s and %s programming language!\n", lang1, lang2)
-	fmt.Println("My current skills:", lang1, lang2 + "!")
+	fmt.Println("My current skills :", lang1, lang2 + "!")
 	fmt.Printf("And also I study %s and %s!\n", goFramework, jsFramework)
+	fmt.Println("I've been working on a project using", *frontend, " and ", *backend)
 }
