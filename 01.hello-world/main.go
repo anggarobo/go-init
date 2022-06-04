10745d5ce07c84c45a4d1e3816d782b889f357ab
anggarobo <anggaprabes@gmail.com>
2022-06-04 23:42:02 +0700

    variable declaration part 2: type interface

--- 01.hello-world/main.go
+++ 01.hello-world/main.go
@@ -3,11 +3,17 @@ package main
 import "fmt"
 
 func main() {
+	// manifest typing
 	var lang1 string = "Go"
 	var lang2 string
 	lang2 = "Javascript"
 
+	// type interface
+	goFramework := "gin gonic"
+	var jsFramework = "react.js"
+
 	fmt.Println("Hello World")
 	fmt.Printf("Hi, My Name is Angga, I'm learning %s and %s programming language!\n", lang1, lang2)
 	fmt.Println("My current skills:", lang1, lang2 + "!")
+	fmt.Printf("And also I study %s and %s!\n", goFramework, jsFramework)
 }
